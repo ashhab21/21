94795868f28ad9a63787440e1c19aee1aec57ff7
ashhab21 <69522694+ashhab21@users.noreply.github.com>
2025-12-29 10:31:03 +0300

    Update player.js

--- player.js
+++ player.js
@@ -1,7 +1,7 @@
 const video=document.getElementById('video');
 const iframe=document.getElementById('iframe');
 const episodesBox = document.querySelector('.episodes');
-if (episodesBox) {const count = episodesBox.querySelectorAll('button').length;if (count > 40) {episodesBox.classList.add('scrollable');}}
+if (episodesBox) {const count = episodesBox.querySelectorAll('button').length;if (count > 20) {episodesBox.classList.add('scrollable');}}
 document.querySelectorAll('.episodes button').forEach(btn=>{btn.onclick=()=>{const src=btn.dataset.src;video.style.display='none';iframe.style.display='none';if(/\.mp4|m3u8|ts/.test(src)){video.src=src;video.style.display='block';}else{iframe.src=src;iframe.style.display='block';}}});
 document.body.style.overflow = 'hidden';
 document.body.style.overflow = '';
